ad728c897d6eb71f59e84e5277f4e754899d159b
yusronhanan <yusronzain@gmail.com>
2017-03-07 05:16:21 +0700

    Add project files.

---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System.Web.Mvc;
+using asp_mvc_2.Security;
+using System.Web.Security;
+using asp_mvc_2.Models.ViewModel;
+using asp_mvc_2.Models.EntityManager;
+
+
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+
+        [AuthorizeRole("Admin")]
+        public ActionResult ManageUserPartial(string status = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager();
+                UserDataView UDV = UM.GetUserDataView(loginName);
+
+                string message = string.Empty;
+                if (status.Equals("update"))
+                    message = "Update Successful";
+                else if (status.Equals("delete"))
+                    message = "Delete Successful";
+
+                ViewBag.Message = message;
+                return PartialView(UDV);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult UpdateUserData(int userID, string loginName, string password, string firstName, string lastName, string gender, int roleID = 0)
+        {
+            UserProfileView UPV = new UserProfileView();
+            UPV.SYSUserID = userID;
+            UPV.LoginName = loginName;
+            UPV.Password = password;
+            UPV.FirstName = firstName;
+            UPV.LastName = lastName;
+            UPV.Gender = gender;
+
+            if (roleID > 0)
+                UPV.LOOKUPRoleID = roleID;
+
+            UserManager UM = new UserManager();
+            UM.UpdateUserAccount(UPV);
+
+            return Json(new { success = true });
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult DeleteUser(int userID)
+        {
+            UserManager UM = new UserManager();
+            UM.DeleteUser(userID);
+            return Json(new { success = true });
+        }
+       
+    }
+}
---
+++ asp_mvc_2/Models/EntityManager/UserManager.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Linq;
+using asp_mvc_2.Models.DB;
+using asp_mvc_2.Models.ViewModel;
+using System.Collections.Generic;
+
+namespace asp_mvc_2.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void DeleteUser(int userID)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                using (var dbContextTransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+
+                      var SUR = db.SYSUserRole.Where(o => o.SYSUserID == userID);
+                        if (SUR.Any())
+                        {
+                            db.SYSUserRole.Remove(SUR.FirstOrDefault());
+                            db.SaveChanges();
+                        }
+
+                        var SUP = db.SYSUserProfile.Where(o => o.SYSUserID == userID);
+                        if (SUP.Any())
+                        {
+                            db.SYSUserProfile.Remove(SUP.FirstOrDefault());
+                            db.SaveChanges();
+                        }
+
+                        var SU = db.SYSUser.Where(o => o.SYSUserID == userID);
+                        if (SU.Any())
+                        {
+                            db.SYSUser.Remove(SU.FirstOrDefault());
+                            db.SaveChanges();
+                        }
+
+                        dbContextTransaction.Commit();
+                    }
+                    catch
+                    {
+                        dbContextTransaction.Rollback();
+                    }
+                }
+            }
+        }
+        public void UpdateUserAccount(UserProfileView user)
+        {
+
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                using (var dbContextTransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+
+                        SYSUser SU = db.SYSUser.Find(user.SYSUserID);
+                        SU.LoginName = user.LoginName;
+                        SU.PasswordEncryptedText = user.Password;
+                        SU.RowCreatedSYSUserID = user.SYSUserID;
+                        SU.RowModifiedSYSUserID = user.SYSUserID;
+                        SU.RowCreatedDateTime = DateTime.Now;
+                        SU.RowMOdifiedDateTime = DateTime.Now;
+
+                        db.SaveChanges();
+
+                        var userProfile = db.SYSUserProfile.Where(o => o.SYSUserID == user.SYSUserID);
+                        if (userProfile.Any())
+                        {
+                            SYSUserProfile SUP = userProfile.FirstOrDefault();
+                            SUP.SYSUserID = SU.SYSUserID;
+                            SUP.FirstName = user.FirstName;
+                            SUP.LastName = user.LastName;
+                            SUP.Gender = user.Gender;
+                            SUP.RowCreatedSYSUserID = user.SYSUserID;
+                            SUP.RowModifiedSYSUserID = user.SYSUserID;
+                            SUP.RowCreatedDateTime = DateTime.Now;
+                            SUP.RowModifiedDateTime = DateTime.Now;
+
+                            db.SaveChanges();
+                        }
+
+                        if (user.LOOKUPRoleID > 0)
+                        {
+                            var userRole = db.SYSUserRole.Where(o => o.SYSUserID == user.SYSUserID);
+                            SYSUserRole SUR = null;
+                            if (userRole.Any())
+                            {
+                                SUR = userRole.FirstOrDefault();
+                                SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                                SUR.SYSUserID = user.SYSUserID;
+                                SUR.IsActive = true;
+                                SUR.RowCreatedSYSUserID = user.SYSUserID;
+                                SUR.RowModifiedSYSUserID = user.SYSUserID;
+                                SUR.RowCreatedDateTime = DateTime.Now;
+                                SUR.RowModifiedDateTime = DateTime.Now;
+                            }
+                            else
+                            {
+                                SUR = new SYSUserRole();
+                                SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                                SUR.SYSUserID = user.SYSUserID;
+                                SUR.IsActive = true;
+                                SUR.RowCreatedSYSUserID = user.SYSUserID;
+                                SUR.RowModifiedSYSUserID = user.SYSUserID;
+                                SUR.RowCreatedDateTime = DateTime.Now;
+                                SUR.RowModifiedDateTime = DateTime.Now;
+                                db.SYSUserRole.Add(SUR);
+                            }
+
+
+                            db.SaveChanges();
+                        }
+                        dbContextTransaction.Commit();
+                    }
+                    catch
+                    {
+                        dbContextTransaction.Rollback();
+                    }
+                }
+            }
+        }
+        public void AddUserAccount(UserSignUpView user)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                SYSUser SU = new SYSUser();
+                SU.LoginName = user.LoginName;
+                SU.PasswordEncryptedText = user.Password;
+                SU.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1; ;
+                SU.RowCreatedDateTime = DateTime.Now;
+                SU.RowMOdifiedDateTime = DateTime.Now;
+                db.SYSUser.Add(SU);
+                db.SaveChanges();
+                SYSUserProfile SUP = new SYSUserProfile();
+                SUP.SYSUserID = SU.SYSUserID;
+                SUP.FirstName = user.FirstName;
+                SUP.LastName = user.LastName;
+                SUP.Gender = user.Gender;
+                SUP.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowCreatedDateTime = DateTime.Now;
+                SUP.RowModifiedDateTime = DateTime.Now;
+                db.SYSUserProfile.Add(SUP);
+                db.SaveChanges();
+                if (user.LOOKUPRoleID > 0)
+                {
+                    SYSUserRole SUR = new SYSUserRole();
+                    SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                    SUR.SYSUserID = user.SYSUserID;
+                    SUR.IsActive = true;
+                    SUR.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowCreatedDateTime = DateTime.Now;
+                    SUR.RowModifiedDateTime = DateTime.Now;
+                    db.SYSUserRole.Add(SUR);
+                    db.SaveChanges();
+                }
+            }
+        }
+        public bool IsLoginNameExist(string loginName)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                return db.SYSUser.Where(o => o.LoginName.Equals(loginName)).Any();
+            }
+        }
+        public string GetUserPassword(string loginName)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                var user = db.SYSUser.Where(o => o.LoginName.ToLower().Equals(loginName));
+                if (user.Any())
+                    return user.FirstOrDefault().PasswordEncryptedText;
+                else
+                    return string.Empty;
+            }
+        }
+        public bool IsUserInRole(string loginName, string roleName)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                SYSUser SU = db.SYSUser.Where(o => o.LoginName.ToLower().Equals(loginName))?.FirstOrDefault();
+                if (SU != null)
+                {
+                    var roles = from q in db.SYSUserRole
+                                join r in db.LOOKUPRole on q.LOOKUPRoleID equals r.LOOKUPRoleID
+                                where r.RoleName.Equals(roleName) && q.SYSUserID.Equals(SU.SYSUserID)
+                                select r.RoleName;
+                    if (roles != null)
+                    {
+                        return roles.Any();
+                    }
+                }
+                return false;
+            }
+        }
+
+        public List<LOOKUPAvailableRole> GetAllRoles()
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                var roles = db.LOOKUPRole.Select(o => new LOOKUPAvailableRole
+                {
+                    LOOKUPRoleID = o.LOOKUPRoleID,
+                    RoleName = o.RoleName,
+                    RoleDescription = o.RoleDescription
+                }).ToList();
+
+                return roles;
+            }
+        }
+
+        public int GetUserID(string loginName)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                var user = db.SYSUser.Where(o => o.LoginName.Equals(loginName));
+                if (user.Any())
+                    return user.FirstOrDefault().SYSUserID;
+            }
+            return 0;
+        }
+        public List<UserProfileView> GetAllUserProfiles()
+        {
+            List<UserProfileView> profiles = new List<UserProfileView>();
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                UserProfileView UPV;
+                var users = db.SYSUser.ToList();
+
+                foreach (SYSUser u in db.SYSUser)
+                {
+                    UPV = new UserProfileView();
+                    UPV.SYSUserID = u.SYSUserID;
+                    UPV.LoginName = u.LoginName;
+                    UPV.Password = u.PasswordEncryptedText;
+
+                    var SUP = db.SYSUserProfile.Find(u.SYSUserID);
+                    if (SUP != null)
+                    {
+                        UPV.FirstName = SUP.FirstName;
+                        UPV.LastName = SUP.LastName;
+                        UPV.Gender = SUP.Gender;
+                    }
+
+                    var SUR = db.SYSUserRole.Where(o => o.SYSUserID.Equals(u.SYSUserID));
+                    if (SUR.Any())
+                    {
+                        var userRole = SUR.FirstOrDefault();
+                        UPV.LOOKUPRoleID = userRole.LOOKUPRoleID;
+                        UPV.RoleName = userRole.LOOKUPRole.RoleName;
+                        UPV.IsRoleActive = userRole.IsActive;
+                    }
+
+                    profiles.Add(UPV);
+                }
+            }
+
+            return profiles;
+        }
+
+        public UserDataView GetUserDataView(string loginName)
+        {
+            UserDataView UDV = new UserDataView();
+            List<UserProfileView> profiles = GetAllUserProfiles();
+            List<LOOKUPAvailableRole> roles = GetAllRoles();
+
+            int? userAssignedRoleID = 0, userID = 0;
+            string userGender = string.Empty;
+
+            userID = GetUserID(loginName);
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                userAssignedRoleID = db.SYSUserRole.Where(o => o.SYSUserID == userID)?.FirstOrDefault().LOOKUPRoleID;
+                userGender = db.SYSUserProfile.Where(o => o.SYSUserID == userID)?.FirstOrDefault().Gender;
+            }
+
+            List<Gender> genders = new List<Gender>();
+            genders.Add(new Gender { Text = "Male", Value = "M" });
+            genders.Add(new Gender { Text = "Female", Value = "F" });
+
+            UDV.UserProfile = profiles;
+            UDV.UserRoles = new UserRoles { SelectedRoleID = userAssignedRoleID, UserRoleList = roles };
+            UDV.UserGender = new UserGender { SelectedGender = userGender, Gender = genders };
+            return UDV;
+        }
+    }
+}
